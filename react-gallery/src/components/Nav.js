a72299fe538f98bcf5e08748b537bfdbd7b2fdff
Jhill161 <Jason161@gmail.com>
2020-01-11 10:39:28 -0800

    Update Nav.js

--- react-gallery/src/components/Nav.js
+++ react-gallery/src/components/Nav.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom'
-
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class Nav extends Component {
-    render() {
-        return(
-            <nav className="main-nav">
-                <ul>
-                <li><NavLink to="/aurora">Aurora</NavLink></li>
-                <li><NavLink to="/alaska">Alaska</NavLink></li>
-                <li><NavLink to="/snow">Snow</NavLink></li>
-                </ul>
-            </nav>
-        )
-    };
-};
+  render() {
+    return (
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <NavLink to="/aurora">Aurora</NavLink>
+          </li>
+          <li>
+            <NavLink to="/alaska">Alaska</NavLink>
+          </li>
+          <li>
+            <NavLink to="/snow">Snow</NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Nav;
